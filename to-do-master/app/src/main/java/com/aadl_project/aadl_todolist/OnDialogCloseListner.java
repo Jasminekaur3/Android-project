17ed98d627c519bbf0788a7502a845aaa29c31df
jasmine <66156115+Jasminekaur3@users.noreply.github.com>
2021-11-01 15:45:18 +0530

    Add files via upload

---
+++ to-do-master/app/src/main/java/com/aadl_project/aadl_todolist/OnDialogCloseListner.java
@@ -0,0 +1,8 @@
+package com.aadl_project.aadl_todolist;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListner {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
